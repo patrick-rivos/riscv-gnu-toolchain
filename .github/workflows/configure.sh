64dc62b697f0887f915bf5eecacd49970c356730
Patrick O'Neill <patrick@rivosinc.com>
2023-09-07 19:28:29 -0700

    Enable gcc checking

--- .github/workflows/configure.sh
+++ .github/workflows/configure.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 
 if [ "$1" == "multilib" ]; then
-  cd tc && cd build && ../configure --prefix=/tc/build --enable-multilib
+  cd tc && cd build && ../configure --prefix=/tc/build --enable-multilib --enable-gcc-checking
     --with-multilib-generator="rv64gc-lp64d--;rv32gc-ilp32d--";
 else
   TARGET_TUPLE=($(echo $2 | tr "-" "\n"))
-  cd tc && cd build && ../configure --prefix=/tc/build \
+  cd tc && cd build && ../configure --prefix=/tc/build --enable-gcc-checking \
     --with-arch=${TARGET_TUPLE[0]} --with-abi=${TARGET_TUPLE[1]};
 fi
