2251b50d94ea67849151f3b736cf405ad89b9986
Patrick O'Neill <patrick@rivosinc.com>
2023-10-06 21:31:08 -0700

    Add repo argument to get_previous_timestamp

--- scripts/get_previous_timestamp.py
+++ scripts/get_previous_timestamp.py
@@ -18,15 +18,21 @@ def parse_arguments():
         type=str,
         help="Github action run id",
     )
+    parser.add_argument(
+        "-repo",
+        required=True,
+        type=str,
+        help="Repo to get runs from",
+    )
     return parser.parse_args()
 
-def get_workflow_runs(token: str):
+def get_workflow_runs(token: str, repo: str):
     params = {"Accept": "application/vnd.github+json",
               "Authorization": f"token {token}",
               "X-GitHub-Api-Version": "2022-11-28",
               "branch": "patchworks-ci",
               "event": "schedule"}
-    url = "https://api.github.com/repos/ewlu/riscv-gnu-toolchain/actions/runs"
+    url = "https://api.github.com/repos/{repo}/actions/runs"
     r = requests.get(url, params)
     runs = json.loads(r.text)
     return runs
@@ -41,7 +47,7 @@ def get_run_timestamp(runs, run_id: str):
 
 def main():
     args = parse_arguments()
-    runs = get_workflow_runs(args.token)
+    runs = get_workflow_runs(args.token, args.repo)
     with open("runs.log", "w") as f:
         f.write(json.dumps(runs['workflow_runs'][:10], indent=4))
     get_run_timestamp(runs['workflow_runs'], args.run_id)
