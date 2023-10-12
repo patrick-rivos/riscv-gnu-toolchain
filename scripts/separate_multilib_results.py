517e1bcf07687791393823d3bf11824d99794465
Edwin Lu <ewlu@rivosinc.com>
2023-10-12 14:21:36 -0700

    added support to separate all multilib results in directory

--- scripts/separate_multilib_results.py
+++ scripts/separate_multilib_results.py
@@ -9,7 +9,13 @@ def parse_arguments():
     parser.add_argument(
         "-file",
         "--file-name",
-        required=True,
+        metavar="<string>",
+        type=str,
+        help="Path to file",
+    )
+    parser.add_argument(
+        "-indir",
+        "--directory-name",
         metavar="<string>",
         type=str,
         help="Path to file",
@@ -37,35 +43,40 @@ def parse_file(file_name: str):
                 cur_target = f"{comps[5]} {comps[6]}"
             target_info[cur_target].append(line)
         summary = f.readline() + f.readline() + f.readline()
-        print(len(target_info))
         for target in target_info.keys():
             target_info[target].append("\n" + summary)
-        print(len(target_info))
         while True:
             line = f.readline()
             if not line:
                 break
             temp = line.split("|")[0].strip().split("/")
             comps = [i.strip() for i in temp]
-            print(comps)
             target = " ".join(comps[0:2])
             target_info[target].append(line)
                 
     return target_info
 
-def main():
-    args = parse_arguments()
-    assert('non-multilib' not in args.file_name)
-    print(args.file_name, args.outdir)
-    targets = parse_file(args.file_name)
-    libc = "linux" if "linux" in args.file_name else "newlib"
-    gcchash = args.file_name.split("-")[4]
+def write_file(file_name: str, outdir: str, indir: str = "./"):
+    targets = parse_file(os.path.join(indir, file_name))
+    libc = "linux" if "linux" in file_name else "newlib"
+    gcchash = file_name.split("-")[4]
     print(targets.keys())
     for target, content in targets.items():
         arch, abi = target.split(" ")
         fname = f"gcc-{libc}-{arch}-{abi}-{gcchash}-multilib-report.log"
-        with open(f"{args.outdir}/{fname}", "w") as f:
+        with open(f"{os.path.join(outdir, fname)}", "w") as f:
             f.writelines(content)
+
+def main():
+    args = parse_arguments()
+    assert(args.directory_name is not None or args.file_name is not None)
+    if args.directory_name is not None:
+        for file in os.listdir(args.directory_name):
+            if "non-multilib" in file:
+                continue
+            write_file(file, args.outdir, args.directory_name)
+    if args.file_name is not None:
+        write_file(args.file_name, args.outdir)
     
 if __name__ == "__main__":
     main()
