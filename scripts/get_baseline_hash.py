8d6c8ea3c75913d4bc06ddca1de03ee7e132d6fa
Edwin Lu <ewlu@rivosinc.com>
2023-10-23 09:56:12 -0700

    switch to using python filtering for issues instead of api filtering

--- scripts/get_baseline_hash.py
+++ scripts/get_baseline_hash.py
@@ -13,27 +13,34 @@ def parse_arguments():
     )
     return parser.parse_args()
 
+def filter_results(issue):
+    issue_labels = issue['labels']
+    labels_check = True
+    filter_labels = ['build-failure', 'testsuite-failure', 'bisect']
+    for label in issue_labels:
+        if label['name'] in filter_labels:
+            labels_check = False
+            break
+    if 'pull_request' not in issue.keys() and labels_check:
+        return True
+
 def parse_baseline_hash(url: str, token: str):
     params = {"Accept": "application/vnd.github+json",
               "Authorization": f"token {token}",
               "X-GitHub-Api-Version": "2022-11-28"}
     r = requests.get(url, params=params)
     issues = json.loads(r.text)
-    issue = None
-    for issue in issues:
-        if 'pull_request' not in issue.keys():
-            break
-
+    filtered = [issue for issue in issues if filter_results(issue)]
+    issue = filtered[0]
     print(f"Baseline from {issue['title']}")
     assert("Testsuite Status" in issue["title"])
     with open("./baseline.txt", "w") as f:
         f.write(issue["title"].split(" ")[-1])
 
-
 def main():
     args = parse_arguments()
-    url = "https://api.github.com/repos/patrick-rivos/gcc-postcommit-ci/issues?page=1&q=is%3Aissue+-label%3Abisect+-label%3Abuild-failure+-label%3Atestsuite-failure&state=all"
-    parse_baseline_hash(url, args.token)
+    all_issues_url = "https://api.github.com/repos/patrick-rivos/gcc-postcommit-ci/issues?state=all&per_page=100"
+    parse_baseline_hash(all_issues_url, args.token)
 
 if __name__ == '__main__':
     main()
