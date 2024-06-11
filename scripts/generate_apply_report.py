39cb9759589e9b5cf21541107154fea2e2b25e61
Patrick O'Neill <patrick@rivosinc.com>
2024-06-11 21:09:42 +0000

    fixup!

--- scripts/generate_apply_report.py
+++ scripts/generate_apply_report.py
@@ -61,6 +61,16 @@ def build_status(bhash:str, thash: str, bstatus: str, tstatus: str):
     result += f"|Tip of tree hash: {thash}|{tstatus}|\n"
     return result
 
+def git_log_wrapper(logfile: str):
+    result = "## Git log\n"
+    result += "git log --oneline from the most recently applied patch to the baseline\n"
+    result += "```\n"
+    result += f"> git log --oneline {hash}^..HEAD\n"
+    with open(logfile, "r") as f:
+        result += f.read()
+    result += "```\n\n"
+    return result
+
 def generate_report(patch_name: str, bhash: str, thash: str, bstatus: str, tstatus: str):
     result = ""
     if bstatus != "pending":
@@ -82,8 +92,7 @@ def generate_report(patch_name: str, bhash: str, thash: str, bstatus: str, tstat
             result += f.read()
         result += "```"
     elif bstatus == "Failed" and tstatus == "Applied":
-        with open("gcc/git_log_tot.txt", "r") as f:
-            result += f.read()
+        git_log_wrapper("gcc/git_log_tot.txt")
         result += "## Notes\n"
         result += f"""Failed to apply to the [post-commit baseline](https://github.com/patrick-rivos/gcc-postcommit-ci/issues?q=is%3Aissue+{bhash}). This can happen
 if your commit requires a recently-commited patch in order to apply.
@@ -95,16 +104,14 @@ different hash, please email us at patchworks-ci@rivosinc.com with a link
 to your patch.
 """
     elif bstatus == "Applied" and tstatus == "Failed":
-        with open("gcc/git_log_bl.txt", "r") as f:
-            result += f.read()
+        git_log_wrapper("gcc/git_log_bl.txt")
         result += "## Notes\n"
         result += """Failed to apply to tip-of-tree. The patch will still
 be tested against the baseline hash. A rebase may be necessary
 before merging.
 """
     else:
-        with open("gcc/git_log_bl.txt", "r") as f:
-            result += f.read()
+        git_log_wrapper("gcc/git_log_bl.txt")
         result += "## Notes\n"
         result += "Patch applied successfully"
 
